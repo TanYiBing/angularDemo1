be85755fe2649b89dd80f60fe3166840edef49e8
TanYiBing <820567197@qq.com>
2018-09-04 16:40:40 +0800

    所有组件静态样式组合完毕

--- src/app/components/stars/stars.component.ts
+++ src/app/components/stars/stars.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
-
+  @Input()
+  private rating: number;
+  private stars: boolean[];
   constructor() { }
 
   ngOnInit() {
+    this.stars = [];
+    for (let i = 0; i < 5; i++) {
+      this.stars.push(i > this.rating);
+    }
   }
 
 }
