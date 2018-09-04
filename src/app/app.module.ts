f865f6aacee449fa6b040e1104f394df2c2d5488
TanYiBing <820567197@qq.com>
2018-09-04 17:39:27 +0800

    创建商品详情页路由，并配置

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -8,6 +8,14 @@ import { SearchComponent } from './components/search/search.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { ProductComponent } from './components/product/product.component';
 import { StarsComponent } from './components/stars/stars.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product/:prodTitle', component: ProductDetailComponent},
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
@@ -17,10 +25,16 @@ import { StarsComponent } from './components/stars/stars.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarsComponent
+    StarsComponent,
+    HomeComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
